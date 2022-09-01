cb5c52b65ac670f369b223747226fddee5ab04f1
KHAIRUL ISLAM <khairulislam38@gmail.com>
2022-09-02 01:01:54 +0600

    update

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
+const players = require("./data/players");
+const tags = require("./data/tags");
 const todos = require("./data/todos");
+const transactions = require("./data/transactions");
 const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
-const transactions = require("./data/transactions");
+const videos3 = require("./data/videos3");
 
 module.exports = {
+  players,
+  tags,
   todos,
+  transactions,
   videos,
   videos2,
-  transactions,
-}
-
+  videos3,
+};
